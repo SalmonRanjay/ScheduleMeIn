7b9293f4f17582821d304d1abf51359e8b4f452f
ranjay <ranjay@clearlyinnovative.com>
2016-03-26 07:57:35 -0400

    updated Create Events

--- ScheduleME/CreateEventViewController.swift
+++ ScheduleME/CreateEventViewController.swift
@@ -7,16 +7,61 @@
 //
 
 import UIKit
+import EventKit
 
 class CreateEventViewController: UIViewController {
     
+    @IBOutlet weak var titleTextField:UITextField!;
+    @IBOutlet weak var dateTextField:UITextField!;
+    @IBOutlet weak var backgroundView:UIView!;
+    
     var authInfo: GTMOAuth2Authentication = GTMOAuth2Authentication();
     var dictionaryData = NSMutableDictionary()
+    
+    let eventMaker = GTLCalendarEventCreator();
+    let event = GTLCalendarEvent()
+    // ...
+    
+    var calendars: [EKCalendar]?
+    // 1
+    let eventStore = EKEventStore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         dictionaryData = authInfo.parameters;
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        self.backgroundView.backgroundColor = UIColor(red: 51/255, green: 153/255, blue: 255/255, alpha: 1);
+        
+        checkCalendarAuthorizationStatus();
+        
+        for calendar in self.calendars!{
+        
+            print(calendar);
+        }
+
+    }
+    
+    func checkCalendarAuthorizationStatus() {
+        let status = EKEventStore.authorizationStatusForEntityType(EKEntityType.Event)
+        
+        switch (status) {
+        case EKAuthorizationStatus.NotDetermined:
+            // This happens on first-run
+            requestAccessToCalendar()
+            break;
+        case EKAuthorizationStatus.Authorized:
+            // Things are in line with being able to show the calendars in the table view
+            loadCalendars()
+            break
+        case EKAuthorizationStatus.Restricted, EKAuthorizationStatus.Denied:
+            // We need to help them give us permission
+            //needPermissionView.fadeIn()
+            break
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +69,29 @@ class CreateEventViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: - Request access to calendar
+    // ...
+    
+    func requestAccessToCalendar() {
+        eventStore.requestAccessToEntityType(EKEntityType.Event, completion: {
+            (accessGranted: Bool, error: NSError?) in
+            
+            if accessGranted == true {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.loadCalendars()
+                                   })
+            } else {
+                dispatch_async(dispatch_get_main_queue(), {
+                                    })
+            }
+        })
+    }
+    
+    
+    
+    func loadCalendars() {
+        self.calendars = eventStore.calendarsForEntityType(EKEntityType.Event)
+    }
 
     /*
     // MARK: - Navigation
@@ -34,6 +102,18 @@ class CreateEventViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    @IBAction func goToSettings(sender: AnyObject){
+    
+        let openSettingsUrl = NSURL(string: UIApplicationOpenSettingsURLString)
+        UIApplication.sharedApplication().openURL(openSettingsUrl!)
+    
+    }
+    
+    @IBAction func createEvent(sender: AnyObject){
+    
+    }
     
     
     @IBAction func submitEvent(sender: AnyObject) {
